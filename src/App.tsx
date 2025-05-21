a9f61b6f41cd5f1a7042ff5c07548a3e1ca635bd
blacknesss <hikky93@mail.ru>
2025-05-21 06:46:22 +0300

    added themes, context for theme and useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { Link } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 export default function App() {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/' element={<MainPageLazy/>}/>
-                    <Route path='/about' element={<AboutPageLazy/>}/>
+                    <Route path='/' element={<MainPageLazy />} />
+                    <Route path='/about' element={<AboutPageLazy />} />
                 </Routes>
             </Suspense>
         </div>
