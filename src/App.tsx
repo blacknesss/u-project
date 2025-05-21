db0d7939c38f032808be40cc43a36474cfcd9fe8
blacknesss <hikky93@mail.ru>
2025-05-21 05:14:37 +0300

    added react-lazy and set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import Counter from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import { Suspense } from 'react';
 
 export default function App() {
     return (
-        <div>
-            <Counter />
+        <div className='app'>
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPageLazy/>}/>
+                    <Route path='/about' element={<AboutPageLazy/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
