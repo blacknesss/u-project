d5528f5cd92be30428987d03a04875e3ea111cae
blacknesss <hikky93@mail.ru>
2025-05-26 10:35:22 +0300

    added sidebar and replaced theme-switcher into sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,14 +1,18 @@
-import { Route } from "react-router-dom";
-import { Suspense } from "react";
-import { Routes } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { Route } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 export function AppRouter() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                {Object.values(routeConfig).map(({path, element}) => (
-                    <Route key={path} path={path} element={element}/>
+                {Object.values(routeConfig).map(({ path, element }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={<div className='page-wrapper'>{element}</div>}
+                    />
                 ))}
             </Routes>
         </Suspense>
